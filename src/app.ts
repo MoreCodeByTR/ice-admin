e3751f0f48eea9998408c5b6ef3f871c96bb28e7
田睿 <761170536@qq.com>
2023-02-19 17:20:27 +0800

    chore: 更改路由 basename

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,7 @@ import { defineAppConfig } from 'ice';
 // App config, see https://v3.ice.work/docs/guide/basic/app
 export default defineAppConfig(() => ({
   // Set your configs here.
+  router: {
+    basename: '/ice-admin/',
+  },
 }));
