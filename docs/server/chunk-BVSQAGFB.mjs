e0f0261d160bd0fc19fd6224ed9babab5beb1e47
田睿 <761170536@qq.com>
2023-02-19 17:50:28 +0800

    feat: 更换ico & logo

---
+++ docs/server/chunk-BVSQAGFB.mjs
@@ -0,0 +1,16 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+
+// .ice/index.ts
+import { Link, Outlet, useParams, useSearchParams, useLocation, useNavigate } from "@ice/runtime/router";
+import { defineAppConfig, useAppData, useData, useConfig, Meta, Title, Links, Scripts, Data, Main, history, KeepAliveOutlet, useMounted, ClientOnly, withSuspense, useSuspenseData, defineDataLoader, defineServerDataLoader, defineStaticDataLoader } from "@ice/runtime";
+
+export {
+  defineAppConfig,
+  Meta,
+  Title,
+  Links,
+  Scripts,
+  Main,
+  history
+};
+//# sourceMappingURL=chunk-BVSQAGFB.mjs.map
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import logo from '@/assets/logo.png';
+import logo from '@/assets/logo.jpeg';
 import styles from './index.module.css';
 import { history } from 'ice';
 
